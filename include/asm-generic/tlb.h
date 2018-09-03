3b96c3f2d760687acedb51c221137675968e83a0
Peter Zijlstra <peterz@infradead.org>
2018-09-03 15:07:36 +0100

    BACKPORT: mm/memory: Move mmu_gather and TLB invalidation code into its own file
    In preparation for maintaining the mmu_gather code as its own entity,
    move the implementation out of memory.c and into its own file.
    
    [UtsavBalar1231]: Adapt to sm8250 4.19
    
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/asm-generic/tlb.h
+++ include/asm-generic/tlb.h
@@ -287,6 +287,7 @@ void arch_tlb_gather_mmu(struct mmu_gather *tlb,
 void tlb_flush_mmu(struct mmu_gather *tlb);
 void arch_tlb_finish_mmu(struct mmu_gather *tlb,
 			 unsigned long start, unsigned long end, bool force);
+void tlb_flush_mmu_free(struct mmu_gather *tlb);
 void tlb_flush_pmd_range(struct mmu_gather *tlb, unsigned long address,
 			 unsigned long size);
 
