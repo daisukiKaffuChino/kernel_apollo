12872aabd7618cdde6d2dea6aa1d747a7621a3c7
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-28 12:38:10 -0800

    mm: vmpressure: Interpret zero scanned pages as 100% pressure
    When no pages are scanned, it usually means no zones were reclaimable
    and nothing could be done. In this case, the reported pressure should be
    100 to elicit help from any listeners. This fixes the vmpressure
    framework not working when memory pressure is very high.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -385,35 +385,38 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned, bool critical,
 	unsigned long pressure;
 	unsigned long stall;
 
-	if (!scanned)
-		return;
+	if (scanned) {
+		spin_lock(&vmpr->sr_lock);
+		if (!vmpr->scanned)
+			calculate_vmpressure_win();
+
+		vmpr->scanned += scanned;
+		vmpr->reclaimed += reclaimed;
 
 		if (!current_is_kswapd())
 			vmpr->stall += scanned;
 
-	vmpr->scanned += scanned;
-	vmpr->reclaimed += reclaimed;
+		stall = vmpr->stall;
+		scanned = vmpr->scanned;
+		reclaimed = vmpr->reclaimed;
+		spin_unlock(&vmpr->sr_lock);
 
-	if (!current_is_kswapd())
-		vmpr->stall += scanned;
-
-	stall = vmpr->stall;
-	scanned = vmpr->scanned;
-	reclaimed = vmpr->reclaimed;
-	spin_unlock(&vmpr->sr_lock);
-
-		if (!critical && scanned < calculate_vmpressure_win()) {
-			spin_unlock_irqrestore(&vmpr->sr_lock, flags);
+		if (scanned < vmpressure_win)
 			return;
-		}
 	}
+
+	spin_lock(&vmpr->sr_lock);
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
 	vmpr->stall = 0;
 	spin_unlock(&vmpr->sr_lock);
 
-	pressure = vmpressure_calc_pressure(scanned, reclaimed);
-	pressure = vmpressure_account_stall(pressure, stall, scanned);
+	if (scanned) {
+		pressure = vmpressure_calc_pressure(scanned, reclaimed);
+		pressure = vmpressure_account_stall(pressure, stall, scanned);
+	} else {
+		pressure = 100;
+	}
 	vmpressure_notify(pressure);
 }
 
