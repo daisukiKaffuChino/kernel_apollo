61f9c5429a5c29c5ea3ae9e347517f41183e3961
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-28 12:43:54 -0800

    mm: vmpressure: Don't exclude any allocation types
    Although userspace processes can't directly help with kernel memory
    pressure, killing userspace processes can relieve kernel memory if they
    are responsible for that pressure in the first place. It doesn't make
    sense to exclude any allocation types knowing that userspace can indeed
    affect all memory pressure, so don't exclude any allocation types from
    the pressure calculations.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -284,7 +284,6 @@ static void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg, bool critical,
 			     unsigned long reclaimed)
 {
 	struct vmpressure *vmpr = memcg_to_vmpressure(memcg);
-	unsigned long flags;
 
 	/*
 	 * If we got here with no pages scanned, then that is an indicator
@@ -386,13 +385,8 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned, bool critical,
 	unsigned long pressure;
 	unsigned long stall;
 
-	if (critical)
-		scanned = calculate_vmpressure_win();
-
-	spin_lock_irqsave(&vmpr->sr_lock, flags);
-	if (scanned) {
-		vmpr->scanned += scanned;
-		vmpr->reclaimed += reclaimed;
+	if (!scanned)
+		return;
 
 		if (!current_is_kswapd())
 			vmpr->stall += scanned;
