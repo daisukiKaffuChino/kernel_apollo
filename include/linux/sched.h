9ccede88eb005ba08181ca31c2542f654d30ddc5
onettboots <blackcocopet@gmail.com>
2025-03-10 06:45:37 +0000

    sched: drop duplicate
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1502,9 +1502,6 @@ struct task_struct {
 #ifdef CONFIG_ANDROID_SIMPLE_LMK
 	struct task_struct		*simple_lmk_next;
 #endif
-#ifdef CONFIG_ANDROID_SIMPLE_LMK
-	struct task_struct		*simple_lmk_next;
-#endif
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
